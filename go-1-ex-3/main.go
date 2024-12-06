a8414f9d7c85d6bb55395b049855d850d895279d
Alessio <alessio.huber3@gmail.com>
2024-12-06 08:46:52 +0100

    Completet Task

--- go-1-ex-3/main.go
+++ go-1-ex-3/main.go
@@ -3,19 +3,33 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
 	"time"
 )
 
 func main() {
-	var eyes = rand.Intn(5) + 1
+	var eyes = rand.Intn(6) + 1 // Simulate a standard 6-sided die
 	var when = time.Now()
 
-	// TODO: use fmt.Fprintln instead!
-	fmt.Println("the dice shows", eyes, "eyes")
+	// Open eyes.txt for writing
+	eyesFile, err := os.Create("eyes.txt")
+	if err != nil {
+		fmt.Println("Error creating eyes.txt:", err)
+		return
+	}
+	defer eyesFile.Close() // Ensure the file is closed after writing
 
-	// TODO: use fmt.Fprintln instead!
-	fmt.Println("the dice was rolled at", when)
+	// Open dice.log for writing
+	logFile, err := os.Create("dice.log")
+	if err != nil {
+		fmt.Println("Error creating dice.log:", err)
+		return
+	}
+	defer logFile.Close() // Ensure the file is closed after writing
 
-	// TODO: how to write the output into eyes.txt and dice.log?
-	// go run ex3/main.go TODO
+	// Use fmt.Fprintln to write to eyes.txt
+	fmt.Fprintln(eyesFile, "The dice shows", eyes, "eyes")
+
+	// Use fmt.Fprintln to write to dice.log
+	fmt.Fprintln(logFile, "The dice was rolled at", when)
 }
