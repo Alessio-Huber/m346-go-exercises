d08ac07e820b0a4c75d600c235d13d1dcb41a16b
Alessio <alessio.huber3@gmail.com>
2024-12-20 14:33:23 +0100

    task completet

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,18 @@
 package main
 
-// TODO: implement the function computeGrade
+import "fmt"
+
+// computeGrade berechnet die Note basierend auf der erreichten Punktzahl und der maximalen Punktzahl.
+func computeGrade(gotPoints float64, maxPoints float64) float64 {
+	if maxPoints == 0 {
+		return 6.0 // Wenn die maximale Punktzahl 0 ist, geben wir die schlechteste Note zurück.
+	}
+	grade := 5/maxPoints*gotPoints + 1
+	return grade
+}
 
 func main() {
-	// TODO: call the function computeGrade
+	fmt.Println(computeGrade(17.5, 28.0))
+	fmt.Println(computeGrade(25.0, 30.0))
+	fmt.Println(computeGrade(0.0, 20.0))
 }
